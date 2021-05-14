ac5b5bb6fd67a41d1161c6d99e932d18ab4617e3
kawada68 <nodoguro68@gmail.com>
2021-05-14 16:01:00 +0900

    modeの修正 監視モード devServerの設定

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'devlopment',
+    mode: 'development',
     entry: './src/js/app.js',
+    watch: true,
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'js/bundle.js',
+    },
+    devServer: {
+        open: true,
+        port: 9000,
+        contentBase: path.resolve(__dirname, 'public'),
+        watchContentBase: true,
     },
 };
