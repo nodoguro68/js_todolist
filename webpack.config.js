65a298b6596dab82b172ed39211cca9c1b4ea833
kawada68 <nodoguro68@gmail.com>
2021-05-15 22:49:10 +0900

    MiniCssExtractPlugin、HtmlWebpackPlugin読み込み
    sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -13,5 +15,47 @@ module.exports = {
         port: 9000,
         contentBase: path.resolve(__dirname, 'public'),
         watchContentBase: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            url: false,
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require('sass'),
+                            sassOptions: {
+                                fiber: require('fibers'),
+                            },
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/style.css",
+        }),
+        // new HtmlWebpackPlugin({
+        //     filename: 'index.html',
+        // }),
+        
+    ],
+    devtool: "source-map",
+    watchOptions: {
+        ignored: /node_modules/
     },
 };
